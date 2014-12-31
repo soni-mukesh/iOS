7e5bc705eb25bc66a165e2cd918429df4cdb0692
soni-mukesh <mukesh.uhr@gmail.com>
2014-12-31 19:07:44 +0530

    content-type case changes

--- IAmHere/Classes/Others/Common.h
+++ IAmHere/Classes/Others/Common.h
@@ -8,7 +8,7 @@ static NSString* const kLocation = @"Location";
 
 static NSString* const kGET					= @"GET";
 static NSString* const kPOST				= @"POST";
-static NSString* const kContentType			= @"Content-Type";
+static NSString* const kContentType			= @"content-type";
 static NSString* const kContentTypeJson		= @"application/json";
 static NSString* const kContentTypeText     = @"text/plain";
 static NSInteger const kHTTPStatus201		= 201;
